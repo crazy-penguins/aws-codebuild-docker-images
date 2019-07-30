cdc37bceb94772865845b91b72f06191ee8db7ae
2ps <pshingavi@gmail.com>
2019-07-30 09:36:42 -0500

    [OPS-449]  Fixed error in python logic
    Problem?
    
    CODEBUILD_WEBHOOK_TRIGGER is separated by `/` and not by `:`
    
    Solution!
    
    Fix the delimiting character on the call to split.

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ with open('NAME', 'r') as f:
 ref = os.environ['CODEBUILD_WEBHOOK_TRIGGER']
 is_tag = ref.startswith('tag/')
 is_branch = ref.startswith('branch/')
-version = ref.rsplit(':', 1)[-1].strip()
+version = ref.rsplit('/', 1)[-1].strip()
 if is_tag and version.startswith('v'):
     version = version[1:]
 subprocess.run([ 'docker', 'build', '-t', f'{image_name}:{version}', '.' ])
