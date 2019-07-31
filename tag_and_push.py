37940bbe9fd759ccd0c0a834073e3242274210a5
2ps <pshingavi@gmail.com>
2019-07-31 01:48:36 -0500

    [OPS-449]  Fixed tagging / pushing script.

--- tag_and_push.py
+++ tag_and_push.py
@@ -15,14 +15,14 @@ region = os.environ['AWS_DEFAULT_REGION']
 is_tag = ref.startswith('tag/')
 is_branch = ref.startswith('branch/')
 version = ref.rsplit('/', 1)[-1].strip()
-registry_url = f'${account_id}.dkr.ecr.${region}.amazonaws.com'
+registry_url = f'{account_id}.dkr.ecr.{region}.amazonaws.com'
 if is_tag and version.startswith('v'):
     version = version[1:]
 final_tags = [
     f'${registry_url}/{image_name}:latest',
 ]
 if is_tag:
-    final_tags.append(f'${registry_url}/{image_name}:{version}')
+    final_tags.append(f'{registry_url}/{image_name}:{version}')
 for x in final_tags:
     subprocess.run(
         [ 'docker', 'tag', f'{image_name}:{version}', x, ],
